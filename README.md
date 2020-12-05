da0b4cae2d025b6a3dcabec420618799d7b94d9f
Ali Shah <s.ashah@outlook.com>
2020-12-05 00:42:47 -0500

    [Init] First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CS1.1 Assignment 3: OOP Design
+
+Written in: Python 3.7.9
+
+## Description
+
+A ... to practice OOP concepts
---
+++ main.py
@@ -0,0 +1,9 @@
+# Bismillah al-Rahmaan al-Raheem
+# Ali Shah | Dec. 05, 2020
+
+"""CS1.1 Assignment 3: OOP Design."""
+
+import class1
+import class2
+import class3
+import class4
